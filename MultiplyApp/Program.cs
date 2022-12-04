bf608d8e4b0692b1ae9dd580899a1a9b2acc723f
PetrKu09 <petr.ku09@gmail.com>
2022-12-04 12:18:53 +0300

    Change

--- MultiplyApp/Program.cs
+++ MultiplyApp/Program.cs
@@ -4,10 +4,10 @@
     {
         static void Main(string[] args)
         {
-            double a = int.Parse(Console.ReadLine());
-            double b = int.Parse(Console.ReadLine());
+            //double a = int.Parse(Console.ReadLine());
+            //double b = int.Parse(Console.ReadLine());
 
-            Console.WriteLine(a * b);
+            //Console.WriteLine(a * b);
         }
     }
 }
