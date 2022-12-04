6963f617f81e563e20080d1e9aa20d1ba413441d
TheFirest-Artemis <artem.rvachev2@gmail.com>
2022-12-04 11:57:46 +0300

    Initial commit

---
+++ MultiplyApp/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace MultiplyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = int.Parse(Console.ReadLine());
+            double b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(a * b);
+        }
+    }
+}
